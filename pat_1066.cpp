abc04b243a4097975bf383922497d172d69ab1e7
littledou2013 <cchen_xiao_shuang@163.com>
2014-08-06 13:26:45 +0800

    Update pat_1066.cpp

--- pat_1066.cpp
+++ pat_1066.cpp
@@ -103,3 +103,109 @@ int main(void)
   printf("%d\n", head->val);
   return 0;
 }
+/*方法二：
+#include <cstdio>
+#include <iostream>
+
+struct Node
+{
+	int val;
+	Node *left;
+	Node *right;
+	int height;
+	Node(int _val): val(_val), left(NULL), right(NULL),height(1){}
+
+};
+int height(Node *node)
+{
+	if (node == NULL)
+		return 0;
+	else
+		return node->height;
+}
+int max(int a, int b)
+{
+	if (a < b)
+		return b;
+	else
+		return a;
+}
+//左左
+Node *singleRotateWithLeft(Node *k2)
+{
+	Node *k1;
+	k1 = k2->left;
+    k2->left = k1->right;
+    k1->right = k2;
+    k2->height = max(height(k2->right ), height(k2->right)) + 1;
+	k1->height = max(height(k1->left), k2->height) + 1;
+    return k1;  
+}
+Node *singleRotateWithRight(Node *k2)
+{
+	Node *k1;
+	k1 = k2->right;
+    k2->right = k1->left;
+    k1->left = k2;
+    k2->height = max(height(k2->right ), height(k2->right) ) + 1;
+	k1->height = max(k2->height, height(k2->right)) + 1;
+    return k1; 
+}
+Node *doubleRotateWithLeft(Node *k3)
+{
+	k3->left = singleRotateWithRight(k3->left);
+	return singleRotateWithLeft(k3);
+}
+Node *doubleRotateWithRight(Node *k3)
+{
+	k3->right = singleRotateWithLeft(k3->right);
+	return singleRotateWithRight(k3);
+}
+Node *insert(int val, Node * head)
+{
+	if (head == NULL)
+	{
+		head = new Node(val);
+	}
+	else
+	{
+		//插入左子树
+		if (val < head->val)
+		{
+			head->left = insert(val, head->left);
+			if (height(head->left) - height(head->right) == 2)
+				if (val < head->left->val)
+					head = singleRotateWithLeft(head);
+				else
+					head = doubleRotateWithLeft(head);
+
+		}
+		//插入右子树
+		else
+		{
+			head->right = insert(val, head->right);
+			if(height(head->right) - height(head->left) == 2)
+				if (val > head->right->val)
+					head = singleRotateWithRight(head);
+				else
+					head = doubleRotateWithRight(head);
+		}
+	}
+	head->height = max(height(head->left), height(head->right)) + 1;
+	return head;
+}
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
+	int val;
+	Node *head = NULL;
+	for (int i = 0; i < N; ++i)
+	{
+		scanf("%d", &val);
+		head = insert(val, head);
+	}
+	printf("%d\n", head->val);
+	system("pause");
+	return 0;
+}*/
