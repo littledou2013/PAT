7322d35c0ccde6dffd03be58ed44ce8f0445943f
littledou2013 <cchen_xiao_shuang@163.com>
2014-08-11 16:56:50 +0800

    Update pat_1054.cpp

--- pat_1054.cpp
+++ pat_1054.cpp
@@ -31,3 +31,36 @@ int main(void)
 	printf("%d\n", min_node);
 	return 0;
 }
+
+
+方法二
+#include <iostream>
+#include <cstdio>
+#include <map>
+using namespace std;
+
+map<int,int> mp;
+int main(void)
+{
+	int M, N;
+	scanf("%d%d", &M, &N);
+	int val, last;
+	int count = 0;
+	N *= M;
+	scanf("%d", &last);
+	for (int i = 1; i < N; ++i)
+	{
+		scanf("%d", &val);
+		if (last == val)
+			++count;
+		else
+		{
+			--count;
+			if(count <= 0)
+				last = val;
+		}
+	}
+	printf("%d\n", last);
+	system("pause");
+	return 0;
+}
